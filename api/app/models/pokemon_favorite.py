0fb24aaf6304d25dd085cabe44007b28926784af
MarcoRomo17 <utm23090604@utma.edu.mx>
2025-03-21 19:48:18 -0600

    Agregacion del maquetado, incluyendo las tareas

--- api/app/models/pokemon_favorite.py
+++ api/app/models/pokemon_favorite.py
@@ -10,7 +10,7 @@ class PokemonFavorites(SuperClass):
         raise NotImplementedError("No es necesario traer todos los usuarios")
 
     def find_all(self, user_id):#Modificamos el metodo para que filtre ates
-        data=self.collection.find({"user_id": ObjectId(user_id)})
+        data=list(self.collection.find({"user_id": ObjectId(user_id)}))
         for datum in data:
             datum["user_id"]=str(datum['user_id'])
             datum["pokemon_id"]=str(datum['pokemon_id'])
@@ -20,8 +20,7 @@ class PokemonFavorites(SuperClass):
         return data
 
         
-    def find_by_id(self, object_id):
-        raise NotImplementedError("No es necesario traer todos los usuarios")
+
 
     def create(self, data):
         data["user_id"]=ObjectId(data["user_id"])
